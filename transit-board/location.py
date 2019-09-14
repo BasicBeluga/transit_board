7bcf8b4f84896750fc98dcb81a8e5ef33268a51a
Ian Earle <ian@dashhudson.com>
2019-09-14 12:10:46 -0300

    base location stuff

---
+++ transit-board/location.py
@@ -0,0 +1,13 @@
+class LocationHandler():
+    def __init__(self):
+        pass
+
+    def get_location(self):
+        raise Exception("abstraction error")
+
+class MacOSLocationHandler():
+    def __init__(self):
+        pass
+
+    def get_location(self):
+        pass
